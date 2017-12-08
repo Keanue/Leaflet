15ef12e58b39821dacb8548385aab58208171894
Chevrier Quentin <quentin.chevrier@appiway.com>
2017-12-08 15:26:34 +0100

    fixed

--- dist/leaflet-src.js
+++ dist/leaflet-src.js
@@ -1864,7 +1864,7 @@ L.Map = L.Class.extend({
 
 		do {
 			zoom++;
-			boundsSize = this.project(se, zoom).subtract(this.project(nw, zoom)).add(padding);
+			boundsSize = this.project(se, zoom).subtract(this.project(nw, zoom)).add(padding).floor();
 			zoomNotFound = !inside ? size.contains(boundsSize) : boundsSize.x < size.x || boundsSize.y < size.y;
 
 		} while (zoomNotFound && zoom <= maxZoom);
@@ -5054,7 +5054,7 @@ L.Path = (L.Path.SVG && !window.L_PREFER_CANVAS) || !L.Browser.canvas ? L.Path :
 		}
 
 		this._requestUpdate();
-		
+
 		this.fire('remove');
 		this._map = null;
 	},
@@ -8109,7 +8109,7 @@ L.Control.Attribution = L.Control.extend({
 				this.addAttribution(map._layers[i].getAttribution());
 			}
 		}
-		
+
 		map
 		    .on('layeradd', this._onLayerAdd, this)
 		    .on('layerremove', this._onLayerRemove, this);
